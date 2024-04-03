2f141820a2396844d3812a05f4cc63eb5c1c622b
Tyler Nijmeh <tylernij@gmail.com>
2024-04-03 12:49:37 -0700

    Finish cli options
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- bin/unsplash_api.dart
+++ bin/unsplash_api.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:args/args.dart';
+import 'package:http/http.dart';
+import 'package:path/path.dart' as p;
 import 'package:unsplash_api/unsplash_api.dart';
 
 ArgResults _parseArgs(List<String> args) {
@@ -19,15 +21,49 @@ ArgResults _parseArgs(List<String> args) {
       },
     )
     ..addOption(
+      'directory',
+      abbr: 'd',
+      defaultsTo: '.',
+      help: 'The directory path for photo downloads.',
+    )
+    ..addOption(
       'number',
       abbr: 'n',
       defaultsTo: '1',
       help: 'The number of photos to download.',
     )
+    ..addOption(
+      'quality',
+      abbr: 'q',
+      defaultsTo: 'full',
+      allowed: [
+        'smallS3',
+        'thumb',
+        'small',
+        'regular',
+        'full',
+        'raw',
+      ],
+    )
     ..addFlag(
       'watermarked',
       abbr: 'w',
       help: 'Include watermarked (premium or plus) photos.',
+    )
+    ..addFlag(
+      'dry',
+      abbr: 'e',
+      help: 'Dry run; no downloads.',
+    )
+    ..addFlag(
+      'overwrite',
+      abbr: 'r',
+      help: 'Whether or not to overwrite existing downloads.',
+    )
+    ..addFlag(
+      'verbose',
+      abbr: 'v',
+      help: 'Provide logging.',
     );
 
   return argParser.parse(args);
@@ -38,14 +74,43 @@ Future<void> main(List<String> args) async {
   final results = _parseArgs(args);
   final query = results.rest.join(' ');
 
+  final directory = results['directory'] as String;
   final number = int.parse(results['number'] as String);
+  final quality = results['quality'] as String;
+  final dry = results['dry'] as bool;
   final watermarked = results['watermarked'] as bool;
+  final overwrite = results['overwrite'] as bool;
+  final verbose = results['verbose'] as bool;
 
   await getPhotos(
     query,
     number,
     includeWatermarked: watermarked,
-  ).forEach((photo) {
-    stdout.writeln('[*] ${photo.slug}');
+  ).forEach((photo) async {
+    if (verbose) stdout.writeln('[ ] ${photo.slug}');
+
+    // Download the image to disk.
+    if (!dry) {
+      final imageUrlsQualityMap = <String, String>{
+        'smallS3': photo.imageUrls.smallS3,
+        'thumb': photo.imageUrls.thumb,
+        'small': photo.imageUrls.small,
+        'regular': photo.imageUrls.regular,
+        'full': photo.imageUrls.full,
+        'raw': photo.imageUrls.raw,
+      };
+
+      final photoUrl = imageUrlsQualityMap[quality];
+      final uri = Uri.parse(photoUrl!);
+      final image = await get(uri);
+
+      final file = File(p.join(directory, '${photo.slug}.jpeg'));
+      if (!file.existsSync() || overwrite) {
+        await file.writeAsBytes(image.bodyBytes);
+        if (verbose) stdout.writeln('[+] ${photo.slug}');
+      } else {
+        if (verbose) stdout.writeln('[-] ${photo.slug}');
+      }
+    }
   });
 }
