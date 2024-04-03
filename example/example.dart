644fee8f54aaebc3a120da1f782d877d9b3eca50
Tyler Nijmeh <tylernij@gmail.com>
2024-04-03 12:51:56 -0700

    Fix example
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- example/example.dart
+++ example/example.dart
@@ -1,19 +1,14 @@
 import 'dart:io';
 
-import 'package:http/http.dart' as http;
 import 'package:unsplash_api/unsplash_api.dart';
 
 Future<void> main() async {
-  final unsplashImages = await getPhotos('Autumn', 10);
+  final unsplashImages = getPhotos('Autumn', 10);
 
-  for (var image in unsplashImages) {
-    print(image.description);
-    print(image.creationDate);
-    print(image.imageUrls.raw);
-
-    final uri = Uri.parse(image.imageUrls.raw);
-    final response = await http.get(uri);
-    final file = File('${uri.pathSegments.last}.jpeg');
-    await file.writeAsBytes(response.bodyBytes);
+  await for (final image in unsplashImages) {
+    stdout
+      ..writeln(image.description)
+      ..writeln(image.creationDate)
+      ..writeln(image.imageUrls.raw);
   }
 }
